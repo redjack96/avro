cd3fe3fe7f2dc6427185929ae4763e8104d5af6c
Giacomo Lorenzo Rossi <giacomo.redjack@gmail.com>
2021-08-22 19:03:17 +0200

    Analizza le classi con la coverage, ma non crea alcun jacoco.exec

--- lang/java/avro/src/test/java/org/apache/avro/generic/TestGenericData.java
+++ lang/java/avro/src/test/java/org/apache/avro/generic/TestGenericData.java
@@ -17,18 +17,102 @@
  */
 package org.apache.avro.generic;
 
+import org.apache.avro.Conversion;
 import org.apache.avro.Conversions;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+ * Per eseguire i test, usa i comandi seguenti perché molte classi hanno bisogno
+ * di file generati durante la compilazione. mvn -Dtest=TestGenericData test
+ */
 public class TestGenericData {
 
-  private GenericData genericData = new GenericData();
+  private static GenericData genericData;
+
+  @BeforeClass
+  public static void setUpAll() {
+    genericData = new GenericData();
+  }
 
   @Test
   public void addLogicalTypeConversion() {
     genericData.addLogicalTypeConversion(new Conversions.DecimalConversion());
+  }
+
+  @Test
+  public void getConversionByClass() {
+    Conversion<?> conversionByClass = genericData.getConversionByClass(Integer.class);
+    assertNull(conversionByClass);
+  }
+
+  @Test
+  public void genericGetConversionByClass() {
+  }
+
+  @Test
+  public void getConversionFor() {
+  }
+
+  @Test
+  public void setFastReaderEnabled() {
+  }
+
+  @Test
+  public void isFastReaderEnabled() {
+  }
+
+  @Test
+  public void getFastReaderBuilder() {
+  }
+
+  @Test
+  public void createDatumReader() {
+  }
+
+  @Test
+  public void createDatumWriter() {
+  }
+
+  // Questo sembra un buon test.
+  @Test
+  public void validate() {
+  }
+
+  // ANche questo sembra buono, ma cosa fa???
+
+  @Test
+  public void induce() {
+  }
+
+  // Sembra buono
+
+  @Test
+  public void resolveUnion() {
+  }
+
+  // Decente
+  @Test
+  public void getDefaultValue() {
+  }
+
+  // Decente
+  @Test
+  public void deepCopy() {
+  }
+
+  @Test
+  public void newRecord() {
+  }
+
+  @Test
+  public void createString() {
+  }
+
+  @Test
+  public void newArray() {
   }
 
 }
