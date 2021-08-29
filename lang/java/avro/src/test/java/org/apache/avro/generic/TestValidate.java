0cbdd0bef9dc93fab691c0cf2b5ee86ae54805f2
Giacomo Lorenzo Rossi <giacomo.redjack@gmail.com>
2021-08-29 13:51:26 +0200

    Incrementata CC, SC e mutation coverage TestValidate

--- lang/java/avro/src/test/java/org/apache/avro/generic/TestValidate.java
+++ lang/java/avro/src/test/java/org/apache/avro/generic/TestValidate.java
@@ -28,10 +28,8 @@ import org.junit.runners.Parameterized;
 
 import java.math.BigInteger;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 
@@ -80,11 +78,22 @@ public class TestValidate {
   private enum MyEnum{Uno, Due, Tre} // Solo per i parametri
 
   private static Collection<Object[]> configure(){
+    // Inizializzazione variabili aggiuntive per migliorare la coverage (Condition e Statement)
     Schema enumSchema = Schema.createEnum("MyEnum", "", "org.apache.avro.generic.TestValidate", Arrays.asList("Uno", "Due", "Tre"));
     GenericData.EnumSymbol uno = new GenericData.EnumSymbol(enumSchema, MyEnum.Uno);
     ByteBuffer bb = ByteBuffer.wrap(new byte[]{1,23,3,4});
+    Map<String, Long> map = new HashMap<>();
+    map.put("a", 10L);
+    map.put("b", 11L);
+    Schema fixedSchema = Schema.createFixed("fixed", "", "org.apache.avro.generic.TestValidate", 5);
+    GenericData.Fixed f = new GenericData.Fixed(fixedSchema, new byte[]{1,2,3,4,5});
+
+    // Inizializzazione variabili aggiuntive per migliorare la mutation coverage
+    GenericData.Fixed fMut = new GenericData.Fixed(fixedSchema, new byte[]{1,2,3,4,5,6});
+    GenericData.EnumSymbol unoMut = new GenericData.EnumSymbol(enumSchema, "One"); // simbolo non esistente
 
     return Arrays.asList(new Object[][]{
+      // Set di parametri iniziali
       {Schema.create(Schema.Type.STRING), "Stringa", true},
       {Schema.create(Schema.Type.FLOAT), 4, false},
       {Schema.create(Schema.Type.DOUBLE), null, false},
@@ -93,16 +102,31 @@ public class TestValidate {
       {Schema.createMap(Schema.create(Schema.Type.NULL)), Arrays.asList(null, null, null), false},
       {Schema.create(Schema.Type.NULL), null, true},
       {Schema.create(Schema.Type.NULL), new ArrayList<Void>(), false},
+
       // Set di parametri aggiunti per incrementare coverage (statement e condition)
       {ExampleRecord.getSchemaStatic(), new ExampleRecord(), true},
       {ExampleRecord.getSchemaStatic(), new NotARecord(), false},
-      {Schema.createMap(Schema.create(Schema.Type.LONG)), new HashMap<String, Long>(), true},
+      {Schema.createMap(Schema.create(Schema.Type.LONG)), map, true},
       {Schema.createArray(Schema.create(Schema.Type.INT)), new BigInteger("10"), false},
+      {Schema.createArray(Schema.create(Schema.Type.INT)), new Object[]{1, 2, 3L}, false},
       {Schema.create(Schema.Type.BOOLEAN), false, true},
       {Schema.create(Schema.Type.BYTES), new byte[]{1,2,3}, false},
       {Schema.create(Schema.Type.BYTES), bb, true},
+      {Schema.create(Schema.Type.LONG), 1000L, true},
+      {fixedSchema, f, true},
       {enumSchema, MyEnum.Uno, false},
       {enumSchema, uno, true},
+
+      // Set di parametri aggiunti per incrementare mutation coverage
+      {fixedSchema, fMut, false},
+      {fixedSchema, new Object(), false},
+      {enumSchema, unoMut, false},
+      {Schema.create(Schema.Type.STRING), "Stringa".getBytes(StandardCharsets.UTF_8), false},
+      {Schema.create(Schema.Type.INT), "Stringa", false},
+      {Schema.create(Schema.Type.LONG), 1.0f, false},
+      {Schema.create(Schema.Type.FLOAT), 4, false},
+      {Schema.create(Schema.Type.DOUBLE), null, false},
+      {Schema.create(Schema.Type.BOOLEAN), 1, false},
     });
   }
 
